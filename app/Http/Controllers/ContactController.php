cfd2b489ff2bcc667794face925e298b064c7e45
Diogo Cerqueira <contato@diogocerqueira.dev.br>
2024-02-03 14:33:16 -0300

    Add contact http methods

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
+use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\Response;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return Response::json(Contact::where("user_id", $this::$user->id));
+    }
+
+    public function store(ContactRequest $request)
+    {
+        $contact          = new Contact;
+        $contact->name    = $request->name;
+        $contact->contact = $request->contact;
+        $contact->email   = $request->email;
+        $contact->save();
+
+        return Response::json($contact);
+    }
+
+    public function update(ContactRequest $request, int $id)
+    {
+        $contact = Contact::where("id", $id)->where("user_id", $this::$user->id)->first();
+        if (!$contact) return Response::json(["message" => "Contact not found"], HttpResponse::HTTP_NOT_FOUND);
+
+        $contact->name    = $request->name;
+        $contact->contact = $request->contact;
+        $contact->email   = $request->email;
+        $contact->save();
+
+        return Response::noContent();
+    }
 }
