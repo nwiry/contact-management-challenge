56cdbcee01d8f056eee2f8843764e2dd79c8864f
Diogo Cerqueira <contato@diogocerqueira.dev.br>
2024-02-03 14:45:59 -0300

    Add form validation

---
+++ app/Http/Requests/ContactRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name"    => "required|min:5|max:60",
+            "contact" => "required|min:9|max:9",
+            "email"   => "required|email|max:255",
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            "message" => "failed validation",
+            "errors" => $validator->errors()
+        ], Response::HTTP_BAD_GATEWAY));
+    }
+}
