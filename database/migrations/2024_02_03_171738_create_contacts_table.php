264311cb7e4b9b19bf483d6aeb9c4f49806befdf
Diogo Cerqueira <contato@diogocerqueira.dev.br>
2024-02-03 14:20:26 -0300

    Added Contacts Table and Controller

---
+++ database/migrations/2024_02_03_171738_create_contacts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->string("name", 60)->collation("utf8mb4_general_ci");
+            $table->string("contact", 20)->unique();
+            $table->string("email", 255)->collation("utf8mb4_general_ci")->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
